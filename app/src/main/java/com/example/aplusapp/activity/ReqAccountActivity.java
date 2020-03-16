294d0859dea33d72d2d84064126a5e54155f6704
Shalitha <shalithax@gmail.com>
2020-03-16 12:33:30 +0530

    add spinner

--- app/src/main/java/com/example/aplusapp/activity/ReqAccountActivity.java
+++ app/src/main/java/com/example/aplusapp/activity/ReqAccountActivity.java
@@ -3,19 +3,27 @@ package com.example.aplusapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.example.aplusapp.R;
-import com.jaredrummler.materialspinner.MaterialSpinner;
+
+import fr.ganfra.materialspinner.MaterialSpinner;
 
 public class ReqAccountActivity extends AppCompatActivity {
 
+    MaterialSpinner spinner;
+    String[] ITEMS = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_req_account);
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ITEMS);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+        spinner = (MaterialSpinner) findViewById(R.id.spinnerRole);
+        spinner.setAdapter(adapter);
 
     }
 }
