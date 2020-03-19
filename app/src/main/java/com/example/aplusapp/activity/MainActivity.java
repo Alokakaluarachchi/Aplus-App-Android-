c36cde56d48c1061774da66f298ebcc129533637
ravindu <it17137638@my.sliit.lk>
2020-03-19 15:04:31 +0530

    Cashier Management Design.Same as the Order Management Design.Not the finalized one.

--- app/src/main/java/com/example/aplusapp/activity/MainActivity.java
+++ app/src/main/java/com/example/aplusapp/activity/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
             }
         });
 
