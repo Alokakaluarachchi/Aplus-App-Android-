811374275e154ef024d4de25e444a541bc8b5a88
Shalitha <shalithax@gmail.com>
2020-04-14 23:27:12 +0530

    add password reset dialog

--- app/src/main/java/com/example/aplusapp/activity/ForgotPasswordResetPopup.java
+++ app/src/main/java/com/example/aplusapp/activity/ForgotPasswordResetPopup.java
@@ -1,9 +1,6 @@
 package com.example.aplusapp.activity;
 
 import android.app.Dialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -11,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,47 +16,32 @@ import androidx.fragment.app.DialogFragment;
 
 import com.example.aplusapp.R;
 
-public class ForgotPasswordPopup extends DialogFragment {
+public class ForgotPasswordResetPopup extends DialogFragment {
 
-    private TextView btnClose;
-    private Button btnReset;
+    Button btnReset;
+    EditText txtToken, txtPassword, txtPasswordConfirm;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //code
-        //dismiss();
-        btnClose = (TextView) view.findViewById(R.id.btnClose);
         btnReset = view.findViewById(R.id.btnReset);
-
-        btnClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dismiss();
-            }
-        });
-
-        btnReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
+        txtToken = view.findViewById(R.id.txtResetCode);
+        txtPassword = view.findViewById(R.id.txtPassword);
+        txtPasswordConfirm = view.findViewById(R.id.txtResetPassword);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.forget_pw_popup, container, false);
+        return inflater.inflate(R.layout.new_passowrd_popup, container, false);
     }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         Dialog dialog = new Dialog(getActivity(), android.R.style.Theme_Translucent_NoTitleBar);
-        final View view = getActivity().getLayoutInflater().inflate(R.layout.forget_pw_popup, null);
+        final View view = getActivity().getLayoutInflater().inflate(R.layout.new_passowrd_popup, null);
 
         //final Drawable d = new ColorDrawable(Color.BLACK);
         //d.setAlpha(130);
@@ -75,5 +57,6 @@ public class ForgotPasswordPopup extends DialogFragment {
         dialog.setCanceledOnTouchOutside(true);
 
         return dialog;
+
     }
 }
