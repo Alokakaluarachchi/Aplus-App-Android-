e9d03fd42b8b3ed5f26273b61d2300e1d258a1b0
Aloka <alokakaluarachchi@gmail.com>
2020-04-16 21:07:39 +0530

    implemented datatables

--- app/src/main/java/com/example/aplusapp/db/RoomDatabase.java
+++ app/src/main/java/com/example/aplusapp/db/RoomDatabase.java
@@ -6,16 +6,19 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.aplusapp.db.dao.InventoryDao;
 import com.example.aplusapp.db.dao.UserDao;
+import com.example.aplusapp.model.Inventory;
 import com.example.aplusapp.model.Users;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {Users.class}, version = 1, exportSchema = false)
+@Database(entities = {Users.class, Inventory.class}, version = 1, exportSchema = false)
 abstract class GeneralRoomDatabase extends RoomDatabase {
 
     public abstract UserDao userDao();
+    public abstract InventoryDao inventoryDao();
 
     private static volatile GeneralRoomDatabase INSTANCE;
     private static final int NUMBER_OF_THREADS = 4;
