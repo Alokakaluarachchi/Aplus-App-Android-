9fe4cc9e41dcea6a21a396e14eb7809947b51469
ravindu <it17137638@my.sliit.lk>
2020-04-19 16:12:03 +0530

    Implemented DB

--- app/src/main/java/com/example/aplusapp/db/repos/OrderRepository.java
+++ app/src/main/java/com/example/aplusapp/db/repos/OrderRepository.java
@@ -1,4 +1,39 @@
 package com.example.aplusapp.db.repos;
 
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.aplusapp.db.GeneralRoomDatabase;
+import com.example.aplusapp.db.dao.OrderDao;
+import com.example.aplusapp.model.Order;
+
+import java.util.List;
+
 public class OrderRepository {
+
+    private OrderDao orderDao;
+
+
+    public OrderRepository(Application application){
+
+        GeneralRoomDatabase db = GeneralRoomDatabase.getDatabase(application);
+        orderDao = db.orderDao();
+    }
+
+    public void insertOrder(Order order){
+
+        GeneralRoomDatabase.databaseWriteExecutor.execute(() -> {orderDao.insertOrder(order);});
+
+    }
+
+    public void removeOrder(int id){
+
+        GeneralRoomDatabase.databaseWriteExecutor.execute(() -> {orderDao.removeByID(id);});
+    }
+
+    public void updateOrder(Order order){
+
+        GeneralRoomDatabase.databaseWriteExecutor.execute(() -> {orderDao.updateOrder(order);});
+    }
 }
--- app/src/main/java/com/example/aplusapp/model/Cashier.java
+++ app/src/main/java/com/example/aplusapp/model/Cashier.java
@@ -1,4 +1,57 @@
 package com.example.aplusapp.model;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "cashier")
 public class Cashier {
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    private int ID;
+
+    @NonNull
+    private String Items;
+
+    @NonNull
+    private int Qty;
+
+    @NonNull
+    private String Price;
+
+    private boolean IsActive;
+
+    public Cashier(@NonNull int ID, @NonNull String Items, @NonNull int Qty, @NonNull String Price, boolean IsActive) {
+
+        this.ID = ID;
+        this.Items = Items;
+        this.Qty = Qty;
+        this.Price = Price;
+        this.IsActive = IsActive;
+
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    @NonNull
+    public String getItems() {
+        return Items;
+    }
+
+    @NonNull
+    public int getQty() {
+        return Qty;
+    }
+
+    @NonNull
+    public String getPrice() {
+        return Price;
+    }
+
+    public boolean getIsActive() {
+        return IsActive;
+    }
 }
