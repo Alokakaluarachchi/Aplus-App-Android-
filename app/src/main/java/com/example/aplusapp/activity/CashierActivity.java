5d144f4bbdddf01cbd4a8e2e208fbb101faa676b
ravindu <it17137638@my.sliit.lk>
2020-04-20 11:42:31 +0530

    Implementing the functionalities of Buttons

--- app/src/main/java/com/example/aplusapp/activity/CashierActivity.java
+++ app/src/main/java/com/example/aplusapp/activity/CashierActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
@@ -18,17 +20,34 @@ import com.google.android.material.tabs.TabLayout;
 
 public class CashierActivity extends Fragment {
 
-    Toolbar toolbar;
-    TabLayout tabLayout;
-    TabItem tabChats;
-    TabItem tabStatus;
-    TabItem tabCalls;
-
+    private Button btnPlaceOrder,btnDeleteOrder;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_cashier, container, false);
+
+
+        Button btnPlaceOrder = rootView.findViewById(R.id.btnPlaceOrder);
+        Button btnCancelOrder = rootView.findViewById(R.id.btnCancelOrder);
+
+        btnPlaceOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(),"Order is Placed",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnCancelOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(),"Order is Canceled",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         return rootView;
 
 
