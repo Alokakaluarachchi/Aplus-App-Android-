f5e26ecf5041b3abb122d22ca3261978c44adc93
Aloka <alokakaluarachchi@gmail.com>
2020-04-29 18:43:05 +0530

    implemented update part

--- app/src/main/java/com/example/aplusapp/adepter/InventoryAdapter.java
+++ app/src/main/java/com/example/aplusapp/adepter/InventoryAdapter.java
@@ -1,11 +1,14 @@
 package com.example.aplusapp.adepter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.aplusapp.R;
 import com.example.aplusapp.model.Inventory;
 import com.example.aplusapp.model.User;
 
@@ -14,25 +17,37 @@ import java.util.List;
 public class InventoryAdapter extends RecyclerView.Adapter<InventoryAdapter.MyViewHolder>{
     private List<Inventory> inventoryList;
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        public MyViewHolder(@NonNull View itemView) {
-            super(itemView);
-        }
+        public TextView inventoryName , inventoryId;
+        public MyViewHolder(@NonNull View view) {
+            super(view);
+            inventoryName = (TextView) view.findViewById(R.id.inventoryName);
+            inventoryId = (TextView) view.findViewById(R.id.inventoryId);
+            }
+    }
+
+    public InventoryAdapter(List<Inventory> _inventoryList) {
+        inventoryList = _inventoryList;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.inventory_list, parent, false);
+        return new InventoryAdapter.MyViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Inventory inventory = inventoryList.get(position);
+        holder.inventoryName.setText(inventory.getInventoryName());
+        holder.inventoryId.setText(inventory.getID());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return inventoryList.size();
     }
 
 
